ef49be512c80e47802a4b43ffd12a4b4ef997013
Gaurav Sharma <gsharma1154@gmail.com>
2024-09-19 12:40:13 +0530

    added \t

--- Chapter3_PracticeSet/05_problem.py
+++ Chapter3_PracticeSet/05_problem.py
@@ -1,2 +1,2 @@
-letter="Dear Harry, this \python\ course is 'nice' till 2nd lecture. \n Will have 'to see' what \"it does\" next?"
+letter="Dear Harry, this \python\ course \tis 'nice' till 2nd lecture. \n Will have 'to see' what \"it does\" next?"
 print(letter)
